1fd6486b67bfc8f83d61118a16e67531d89dd26f
mssmln <m.croci.ly@gmail.com>
2021-04-09 19:03:26 +0200

    created messages,flats,images,users tables

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 80)->nullable();
+            $table->string('surname', 80)->nullable();
+            $table->string('email',200)->unique();
+            $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->date('date_of_birth')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ database/migrations/2021_04_09_162438_create_flats_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFlatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flats', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('overview')->nullable();
+            $table->smallInteger('price');
+            $table->tinyInteger('rooms')->default(1);
+            $table->tinyInteger('beds')->default(1);
+            $table->tinyInteger('baths')->default(1);
+            $table->smallInteger('sqm');
+            $table->string('address');
+            $table->double('lat', 12, 9);
+            $table->double('lng', 13, 9);
+            $table->string('flat_img');
+            $table->boolean('visibility')->default(1);
+            $table->integer('views')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flats');
+    }
+}
