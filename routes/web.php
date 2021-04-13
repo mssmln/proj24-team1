40230c963cfa1ed4e2b92d9ce9d04e138770a081
mssmln <m.croci.ly@gmail.com>
2021-04-13 16:35:29 +0200

    modified web.php route

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,6 @@ Route::prefix('admin')
 ->namespace('Admin') 
 ->middleware('auth') 
 ->group(function (){ 
-    Route::get('/','HomeController@index')->name('home'); 
+    Route::get('/home','HomeController@index')->name('home'); 
     Route::resource('/flat','FlatController'); 
 }); 
