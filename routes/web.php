aba9fedf5e54a0a6edd3d92cb443635db2c250d9
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-04-15 10:59:26 +0200

    Finished Validation message & user form

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'HomeController@index');
 Route::get('/search', 'HomeController@search')->name('search');
 Route::get('/flat', 'HomeController@flat')->name('flat');
-Route::get('/send-message', 'HomeController@message')->name('message');
 Route::post('/flat','HomeController@send_message')->name('send_message');
+Route::get('/send-message', 'HomeController@message')->name('message');
 
 
 
