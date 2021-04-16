79c28f9a9ab1d0690664d76577ae162b34e34c19
marco sartori <marcosartori98@yahoo.com>
2021-04-16 12:35:46 +0200

    start footer guest

--- resources/views/guest/partials/footer.blade.php
+++ resources/views/guest/partials/footer.blade.php
@@ -1,6 +1,92 @@
 <!-- codice html -->
 
 <footer>
+    <section class="footer-main">
+        <div class="footer-main-item">
+            <h3 class="footer-title">
+                informazioni
+            </h3>
+            <ul>
+                <li><a href="#">Come funzione Airbnb</a></li>
+                <li><a href="#">Newsletter</a></li>
+                <li><a href="#">Investitori</a></li>
+                <li><a href="#">Airbnb Plus</a></li>
+                <li><a href="#">Airbnb Luxe</a></li>
+                <li><a href="#">HotelTonight</a></li>
+                <li><a href="#">Airbnb for Work</a></li>
+                <li><a href="#">Grazie agli host puoi</a></li>
+                <li><a href="#">Opportunità di lavoro</a></li>
+                <li><a href="#">Lettera dei fondatori</a></li>
+            </ul>
+        </div>
+        {{-- / FOOTER MAIN ITEM --}}
+
+        <div class="footer-main-item">
+            <h3 class="footer-title">
+                community
+            </h3>
+            <ul>
+                <li><a href="#">Diversità e appartenenza</a></li>
+                <li><a href="#">Accessibilità</a></li>
+                <li><a href="#">Airbnb Associates</a></li>
+                <li><a href="#">Alloggi per l'emergenza</a></li>
+                <li><a href="#">Invitare un ospite</a></li>
+                <li><a href="#">Airbnb.org</a></li>
+            </ul>
+        </div>
+        {{-- / FOOTER MAIN ITEM --}}
+
+        <div class="footer-main-item">
+            <h3 class="footer-title">
+                ospita
+            </h3>
+            <ul>
+                <li><a href="#">Diventa un host</a></li>
+                <li><a href="#">Offri un'esperienza online</a></li>
+                <li><a href="#">Offri un'esperienza</a></li>
+                <li><a href="#">Ospitare responsabilmente</a></li>
+                <li><a href="#">Centro Risorse</a></li>
+                <li><a href="#">Community Center</a></li>
+            </ul>
+        </div>
+        {{-- / FOOTER MAIN ITEM --}}
+
+        <div class="footer-main-item">
+            <h3 class="footer-title">
+                assistenza
+            </h3>
+            <ul>
+                <li><a href="#">La nostra risposta all'emergenza covid-19</a></li>
+                <li><a href="#">Centro Assistenza</a></li>
+                <li><a href="#">Opzioni di cancellazione</a></li>
+                <li><a href="#">Servizio di supporto al vicinato</a></li>
+                <li><a href="#">Affidabilità e sicurezza</a></li>
+            </ul>
+        </div>
+        {{-- / FOOTER MAIN ITEM --}}
+        
+    </section>
+    {{-- / FOOTER MAIN --}}
+
+    <section class="footer-bottom">
+        <div class="footer-bottom-item">
+            <ul>
+                <li><i class="far fa-copyright"></i> 2021 Airbnb, Inc</li>
+                <li><a href="#">Privacy</a></li>
+                <li><a href="#">Termini</a></li>
+                <li><a href="#">Mappa del sito</a></li>
+                <li><a href="#">Dettagli dell'azienda</a></li>
+            </ul>
+        </div>
+        {{-- / FOOTER BOTTOM ITEM --}}
+        <div class="footer-bottom-item">
+            <ul>
+                
+            </ul>
+        </div>
+        {{-- / FOOTER BOTTOM ITEM --}}
+    </section>
+    {{-- / FOOTER BOTTOM --}}
 
 
 
