43c2bcd9616e92c36c9dca73a6eb9fd47717b726
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-04-16 12:16:21 +0200

    Little Changes

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -3,47 +3,39 @@
 
 @section('content')
 <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ Route('home') }}">Dashboard</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+@if (Route::has('login'))
+    <div class="top-right links">
+        @auth
+            <a href="{{ Route('home') }}">Dashboard</a>
+        @else
+            <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}">Register</a>
             @endif
+        @endauth
+    </div>
+@endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    AIR Vigorsol
-                </div>
+<div class="content">
+    <div class="title m-b-md">
+        <h1>AIRBNB Vigorsol</h1>
+    </div>
 
-                <div class="links">
-                    <a href="{{route('search')}}">Ricerca</a>
-                    <a href="{{route('flat')}}">Appartamenti</a>
-                    {{-- <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a> --}}
-                </div>
-            </div>
-        </div>
+    <div class="links">
+        <a href="{{route('search')}}">Ricerca</a>
+        <a href="{{route('flat')}}">Appartamenti</a>
+    </div>
+</div>
 
 
 
 <div class="input-group input-group-sm mb-3">
-            <div class="input-group-prepend">
-                <span class="input-group-text" id="inputGroup-sizing-sm">Small</span>
-            </div>
-            <input v-model="query" type="text" class="form-control" >
-        </div>
+    <div class="input-group-prepend">
+        <span class="input-group-text" id="inputGroup-sizing-sm">Ricerca</span>
+    </div>
+    <input v-model="query" type="text" class="form-control">
+</div>
 
 
 <!-- flats -->
@@ -52,17 +44,9 @@
     @php //la @ serve per stampare vue data con blade  @endphp
         <h2> @{{ flat.title }}</h2> 
         <img :src="'storage/' + flat.flat_img" alt="flat.flat_img">
-    
-    
     </div>
-
-
 </div>
 
-
-
-
-<!-- / flats -->
 
 
 @endsection
