ff12828c3dc743f33a710a63cd95c12511a60ffc
Alex <alexmikhajlovic@gmail.com>
2021-04-19 10:38:26 +0200

    Edited Dashboard (admin)

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -3,105 +3,81 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <!-- Title -->
     <title>@yield('title')</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
+
     <!-- Font Awesome CDN -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+
     <!-- Chart.js CDN -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js"></script>
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Favicon -->
+    <link rel="icon" href="https://seeklogo.com/images/A/airbnb-logo-1D03C48906-seeklogo.com.png" sizes="16x16 24x24 32x32 48x48 64x64 128x128">
+
+
 </head>
-<body>
+<body style="background-color: #121212">
 
-    <header class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                <i class="fab fa-airbnb"></i>
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
-
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
-                    <!-- Authentication Links -->
-                    @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name . ' ' . Auth::user()->surname }} 
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                   <!-- Log Out -->
-                                   <i class="fas fa-sign-out-alt"></i>
-                                   Log out
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-                </ul>
-            </div>
-        </div>
-    </header>
+    <!-- Authentication Links -->
+    <ul>
+        @guest
+            <li>
+                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+            </li>
+            @if (Route::has('register'))
+                <li>
+                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                </li>
+            @endif
+        @endguest
+    </ul>
       
     <div id="dashboard">
         <nav id="side_bar">
             <!-- High -->
             <ul>
                 <li>
+                    <a href="{{ url('/') }}" target="_blank" rel="noopener noreferrer">
+                        <img src="https://seeklogo.com/images/A/airbnb-logo-1D03C48906-seeklogo.com.png" width="35rem" alt="">
+                    </a>
+                </li>
+                <li>
                     <a href="{{ route('home') }}">
                         <i class="fas fa-house-user"></i>
                     </a>
                 </li>
                 <li>
-                    <a href="{{Route('flat.index')}}">
+                    <a href="{{ route('flat.index') }} ">
                         <i class="fas fa-home"></i>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a href="">
                         <i class="fas fa-chart-line"></i>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a href="">
                         <i class="fas fa-comments"></i>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a href="">
                         <i class="fas fa-charging-station"></i>
                     </a>
                 </li>
@@ -110,16 +86,32 @@
             <!-- Low -->
             <ul>
                 <li>
-                    <a href="#">
-                        <i class="fas fa-cog"></i>
+                    <a href="">
+                        <i class="fas fa-cog rotate-item"></i>
                     </a>
                 </li>
+                <li>
+                    <a href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                        <i class="fas fa-sign-out-alt"></i>
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </li>
+                <li>
+                    <small id="clock"></small>
+                </li>
+                <!-- <li id="time"></li> -->
             </ul>
         </nav>
     
-        <section id="app">
+        <section id="app" class="dashboard-content">
             @yield('content')
         </section>
     </div>
+
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </body>
 </html>
