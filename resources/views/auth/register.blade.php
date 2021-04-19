b3fb798d0059b37a4dcac02338750cc264ace8c8
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-04-19 23:46:28 +0200

    Edited Homepage_guest - Login_page - Register_page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,104 +2,78 @@
 
 @section('content')
 <div class="register_page">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="webflow_style_input">
-                            {{-- <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label> --}}
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="Name">
-
-                                @error('name')
-                                    <div class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </div>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="webflow_style_input">
-                            {{-- <label for="surname" class="col-md-4 col-form-label text-md-right">{{ __('Surname') }}</label> --}}
-
-                            <div class="col-md-6">
-                                <input id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" required autocomplete="surname" autofocus placeholder="Surname">
-
-                                @error('surname')
-                                    <div class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </div>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="webflow_style_input">
-                            {{-- <label for="date_of_birth" class="col-md-4 col-form-label text-md-right">{{ __('Date of Birth') }}</label> --}}
-
-                            <div class="col-md-6">
-                                <input id="date_of_birth" type="date" class="form-control @error('date_of_birth') is-invalid @enderror" name="date_of_birth" value="{{ old('date_of_birth') }}" required autocomplete="date_of_birth" autofocus>
-
-                                @error('date_of_birth')
-                                    <div class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </div>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="webflow_style_input">
-                            {{-- <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label> --}}
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email">
-
-                                @error('email')
-                                    <div class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </div>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="webflow_style_input">
-                            {{-- <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label> --}}
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Password">
-
-                                @error('password')
-                                    <div class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </div>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="webflow_style_input">
-                            {{-- <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label> --}}
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="Confirm Password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+    <div id="particles_balls">
+        <div v-for="i in 30" class="particle"></div>
     </div>
+
+    <div class="container">
+        
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
+
+            <div class="webflow_style_input">
+                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="Name">
+
+                @error('name')
+                    <div class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </div>
+                @enderror
+            </div>
+
+            <div class="webflow_style_input">
+                <input id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" required autocomplete="surname" autofocus placeholder="Surname">
+
+                @error('surname')
+                    <div class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </div>
+                @enderror
+            </div>
+
+            <div class="webflow_style_input">
+                <input id="date_of_birth" type="date" class="form-control @error('date_of_birth') is-invalid @enderror" name="date_of_birth" value="{{ old('date_of_birth') }}" required autocomplete="date_of_birth" autofocus>
+
+                @error('date_of_birth')
+                    <div class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </div>
+                @enderror
+            </div>
+
+            <div class="webflow_style_input">
+                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email">
+
+                @error('email')
+                    <div class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </div>
+                @enderror
+            </div>
+
+            <div class="webflow_style_input">
+                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Password">
+
+                @error('password')
+                    <div class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </div>
+                @enderror
+            </div>
+
+            <div class="webflow_style_input">
+                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="Confirm Password">
+            </div>
+
+            <div class="checkbox_register">
+                <button type="submit" class="login_button">
+                    {{ __('Registrati') }}
+                </button>
+            </div>
+
+        </form>
+
+    </div>
+
 </div>
 @endsection
