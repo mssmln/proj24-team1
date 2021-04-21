e0a0d43636c72c7446031a7b9cf539b679c0f0f8
Yuri Gianlorenzi <yurigian@gmail.com>
2021-04-21 15:23:17 +0200

    commit

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -11,10 +11,10 @@
             {{-- Input Ricerca --}}
             <div class="input_search_home">
                 <input v-model="query" @keyup="getLanLon" type="text" class="search_home_guest"  placeholder="Dove vuoi andare? ">
-                <button @click="searchWithinRadius">search</button>
                 <div class="flat_list" :class="(query != '') ? 'show_item' : 'hidden_item'">
+                    
                     {{-- Stampa Ricerca --}}
-                    <a href="#" class="flat" v-if="flat.address.toLowerCase().includes(query.toLowerCase())" v-for="(flat,index) in flats"> 
+                    <a :href="'flat/' + flat.slug" class="flat"  v-if="flat.address.toLowerCase().includes(query.toLowerCase())" v-for="(flat,index) in flats"> 
                         <img :src="'storage/' + flat.flat_img" alt="flat.flat_img">
                         <div>
                             <h4> @{{ flat.title }}</h4>
@@ -32,18 +32,15 @@
         </div>
     </div>
 
-
-
     <div class="become_a_host">
-
         <div class="container">
-
-            <h2>Diventa host</h2>
-            <p>Condividi il tuo  spazio per guadagnare qualcosa in più e scoprire nuove opportunità</p>
-            <a href="#">Registrati</a>
-
+            <div class="banner_become">
+                <h2>Diventa host</h2>
+                <p>Condividi il tuo  spazio per guadagnare qualcosa in più e scoprire nuove opportunità.</p>
+                <a href="{{ route('register') }}">Registrati</a>
+            </div>
         </div>
-        
+    
     </div>
 
 
