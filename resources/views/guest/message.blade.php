36de3e129693d7d0640404861191bdff8c478f9a
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-04-23 18:50:25 +0200

    Fix

--- resources/views/guest/message.blade.php
+++ resources/views/guest/message.blade.php
@@ -5,19 +5,6 @@
 @section('content')
 <section class="message_result container fade-in">
 
-
-
-
-    {{-- EVENTUALE SEZIONE DI RIEPILOGO DEL MESSAGGIO INVIATO --}}
-    {{-- <div class="message_content">
-        <p>La tua mail: {{ $message->email }}</p>
-        <p>Riferito all'appartamento: </p>
-        <div class="text_message">
-            <p><i class="far fa-envelope"></i> : {{ $message->message }}</p>
-        </div>
-
-
-    </div> --}}
 </section>
 
 @endsection
