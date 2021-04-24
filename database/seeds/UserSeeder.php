d8e88451ab397db62954a433d38a863b40a910d3
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-04-24 12:04:21 +0200

    Fix

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -14,25 +14,6 @@ class UserSeeder extends Seeder
 
     public function run(Faker $faker)
     {
-
-        $user = [
-            [
-                'name' => 'user',
-                'surname' => 'user',
-                'email' => 'user@mail.com',
-                'password' => '$2y$10$7D9EUbFDAwBWi1E2kno02.t2AYtgOuTg5ol1Bbe06jY14ng7yH7wa',  //password
-            ]
-        ];
-
-        foreach ($user as $value) {
-            $newUser = new User();
-            $newUser->name = $value['name'];
-            $newUser->surname = $value['surname'];
-            $newUser->email = $value['email'];
-            $newUser->password = $value['password'];
-            $newUser->save();
-        }
-
         // for ($i = 0; $i <= 10; $i++) {
 
         //     $newUser = new User();
@@ -45,10 +26,6 @@ class UserSeeder extends Seeder
 
         //     $newUser->save();
         // }
-    }
-
-            $newUser->save();
-        }
     }
 
 }
