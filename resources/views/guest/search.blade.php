066d4297dba05c5a9dd98e17bad2ca11ca4bc038
mssmln <m.croci.ly@gmail.com>
2021-04-28 15:31:00 +0200

    forgot to stage search.blade.php

--- resources/views/guest/search.blade.php
+++ resources/views/guest/search.blade.php
@@ -23,24 +23,6 @@
             </div>
         </div>
 
-<<<<<<< HEAD
-
-
-        <div class="advanced_research">
-            <div class="box_research">
-                <label for="address">Indirizzo</label>
-                <input v-model="query" @keyup="getLanLon" type="text" id="address" placeholder="Via e città (o CAP)">
-            </div>
-            <div class="box_research">
-                <label for="camere">Numero minimo di camere</label>
-                <input type="number" v-model="rooms" id="camere" min="1">
-            </div>
-            <div class="box_research">
-                <label for="letti">Numero minimo di Letti</label>
-                <input type="number" v-model="beds" id="letti" min="1">
-            </div>
-            <button class="search_button" @click="searchWithinRadius">Verifica</button>
-=======
             <div class="advanced_research">
 
                 <div class="box_research box_bigger">
@@ -64,7 +46,6 @@
             <div class="button_end_box">
                 <button class="search_button" @click="searchWithinRadius">Verifica</button>
             </div>
->>>>>>> ec850d8c1e73dfb9af46c8530c529391e922ef7f
         </div>
         {{-- fine box_search_inputs --}}
 
@@ -80,7 +61,6 @@
                         <p>Letti: @{{ result.beds }}</p>
                     </div>
                     <div class="show_price">
-<<<<<<< HEAD
                         <p>@{{ result.price }}</p>
                     </div>
                     <div class="sponsor_layover">
@@ -102,9 +82,6 @@
                     </div>
                     <div class="show_price">
                         <p>35 €</p>
-=======
-                        <p>@{{ result.price }} €</p>
->>>>>>> ec850d8c1e73dfb9af46c8530c529391e922ef7f
                     </div>
                     <div class="sponsor_layover">
                         <span>Mostra</span>
@@ -112,97 +89,6 @@
                     </div>
                 </a>
 
-<<<<<<< HEAD
-                <a class="box_searched_item" href="#">
-                    <img src="https://www.berlino.com/wp-content/uploads/sites/13/Appartamenti.jpg" alt="#">
-                    <h2>Appartamento a villafranca di verona</h2>
-                    <h3>Via Scuderlando 4, 37135 Verona</h3>
-                    <div class="more_info">
-                        <p>Camere: 1</p>
-                        <p>Letti: 2</p>
-                    </div>
-                    <div class="show_price">
-                        <p>35 €</p>
-                    </div>
-                    <div class="sponsor_layover">
-                        <span>Mostra</span>
-
-                    </div>
-                </a>
-
-                <a class="box_searched_item" href="#">
-                    <img src="https://lh3.googleusercontent.com/proxy/EWbZnuglJdypvVO6OPRABizylPLaAkTXMXV0eTfGVIXuC8fGRNk8SZrQBWWziKHIIYAlpUjykXmMqTPp3BWY4vUgsnIh_0buvqBthizHmMdS6iepVnUbdgS13gZD0VdQjAs0wB9sr7sGKhWQGXA" alt="#">
-                    <h2>Secondo appartamento a verona</h2>
-                    <h3>Via Alessandro Sala 4, 37067 Valeggio sul Mincio</h3>
-                    <div class="more_info">
-                        <p>Camere: 3</p>
-                        <p>Letti: 6</p>
-                    </div>
-                    <div class="show_price">
-                        <p>35 €</p>
-                    </div>
-                    <div class="sponsor_layover">
-                        <span>Mostra</span>
-
-                    </div>
-                </a>
-
-                <a class="box_searched_item" href="#">
-                    <img src="https://familygo-c02.kxcdn.com/wp-content/uploads/2020/02/living-jesolo-appartamenti-app-30-sala.jpg" alt="#">
-                    <h2>Primo appartamento a Verona</h2>
-                    <h3>Corso Como 3, 20154 Milano</h3>
-                    <div class="more_info">
-                        <p>Camere: 3</p>
-                        <p>Letti: 4</p>
-                    </div>
-                    <div class="show_price">
-                        <p>35 €</p>
-                    </div>
-                    <div class="sponsor_layover">
-                        <span>Mostra</span>
-
-                    </div>
-                </a>
-
-                <a class="box_searched_item" href="#">
-                    <img src="https://www.ibizalowcost.com/wp-content/uploads/2018/01/living-appartamento-ikebanab3b-ibiza.jpg" alt="#">
-                    <h2>La villa campione al pari di una reggia vieni ora</h2>
-                    <h3>Via Monte Napoleone 4, 20121 Milano</h3>
-                    <div class="more_info">
-                        <p>Camere: 2</p>
-                        <p>Letti: 4</p>
-                    </div>
-                    <div class="show_price">
-                        <p>35 €</p>
-                    </div>
-                    <div class="sponsor_layover">
-                        <span>Mostra</span>
-
-                    </div>
-                </a>
-
-                <a class="box_searched_item" href="#">
-                    <img src="https://www.classcountryhomes.it/wp-content/uploads/2019/05/appartamenti-in-vendita-roma-nord-58.jpg" alt="#">
-                    <h2>Come sentirsi a casa con un appartamento da sogno</h2>
-                    <h3>Via Monte Napoleone 4, 20121 Milano</h3>
-                    <div class="more_info">
-                        <p>Camere: 1</p>
-                        <p>Letti: 2</p>
-                    </div>
-                    <div class="show_price">
-                        <p>35 €</p>
-                    </div>
-                    <div class="sponsor_layover">
-                        <span>Mostra</span>
-
-                    </div>
-                </a> -->
-
-                {{-- proVA PER FORMATTARE -----------------------------------------------------------------------------------------}}
-                {{-- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=DA CANCELLARE=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- --}}
-
-=======
->>>>>>> ec850d8c1e73dfb9af46c8530c529391e922ef7f
             </div>
 
                 <!-- ricerca per rooms e beds  -->
