9ff8a1fe9543db9d17c62e0fba76516cde1e6696
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-04-30 12:56:04 +0200

    Added comments and fixed routes

--- routes/web.php
+++ routes/web.php
@@ -13,25 +13,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Homepage
 Route::get('/', 'HomeController@index')->name('homepage');
+// Pagina ricerca avanzata
 Route::get('/advanced-search', 'HomeController@search')->name('search');
+// Appartamento singolo nel dettaglio con Form per mandare messaggio
 Route::get('/flat/{slug}', 'HomeController@flat')->name('flat');
-Route::get('/sent-message', 'HomeController@message')->name('message');
 Route::post('/flat/{slug}','HomeController@send_message')->name('send_message');
+// Conferma avvenuto invio del messaggio
+Route::get('/sent-message', 'HomeController@message')->name('message');
 
 
-
-
-
+// Rotte di autenticazione Automatiche laravel
 Auth::routes();
 
+// Rotte utente autenticato 
 Route::prefix('admin')->namespace('Admin')->middleware('auth')->group(function () {
+    // Pagina di appoggio Home dashboard
     Route::get('/','HomeController@index')->name('home');
+    // Rotte DashBoard
     Route::resource('/flat','FlatController');
-    Route::get('/flat/sponsor-flat/{id_flat}','HomeController@sponsor')->name('sponsor');
     /* Pagamento */
     Route::get('/flat/{id}/payment', 'PaymentController@index')->name('payment.view');
     Route::post('/flat/{id}/payment/payment-store', 'PaymentController@payment')->name('payment.store');
-
+    // Statistiche Appartamento
     Route::get('/flat/statistics-flat/{id_flat}','HomeController@statistics')->name('statistics');
 }); 
